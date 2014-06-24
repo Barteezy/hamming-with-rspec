e3454c57b2f7e180ea083cce0d5dd0e05f9ffe3b
Luke Bartel <bartelluke@gmail.com>
2014-06-23 21:23:52 -0600

    edited the hamming.rb file, all tests pass

--- lib/hamming.rb
+++ lib/hamming.rb
@@ -1,3 +1,18 @@
 def hamming(strand_1, strand_2)
+  num_char = 0
+  diff = 0
 
+  while num_char != strand_1.length && num_char != strand_2.length
+    if strand_1[num_char] != strand_2[num_char]
+
+      diff = diff + 1
+      num_char = num_char + 1
+    else
+      num_char = num_char + 1
+
+    end
+
+  end
+
+  diff
 end
