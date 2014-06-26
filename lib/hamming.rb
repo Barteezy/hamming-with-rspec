d6d76b32692b5b1dbe34dc466aef906ac7b685b7
Luke Bartel <bartelluke@gmail.com>
2014-06-26 10:24:56 -0600

    all tests pass

--- lib/hamming.rb
+++ lib/hamming.rb
@@ -1,18 +1,14 @@
 def hamming(strand_1, strand_2)
-  num_char = 0
   diff = 0
+  num_char = 0
 
   while num_char != strand_1.length && num_char != strand_2.length
     if strand_1[num_char] != strand_2[num_char]
-
-      diff = diff + 1
-      num_char = num_char + 1
+      diff += 1
+      num_char += 1
     else
-      num_char = num_char + 1
-
+      num_char += 1
     end
-
   end
-
   diff
 end
